41f4c92ec3e15a0e00641bd00dfa185c911a2301
Bruno <bruno.silva.castilho@gmail.com>
2025-03-29 20:47:18 -0300

    feat: added allowedHosts

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['ine5646-programa-o-para-web.onrender.com'],
+  },
 })
