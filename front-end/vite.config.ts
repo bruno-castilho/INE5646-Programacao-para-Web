1b0612913e208e283b48f9bdd2a4a10ad834e036
Bruno <bruno.silva.castilho@gmail.com>
2025-03-29 20:59:52 -0300

    feat!: change allowedHosts

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['ine5646-programa-o-para-web.onrender.com'],
+    allowedHosts: ['https://phpeditor.onrender.com'],
   },
 })
