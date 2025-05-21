9ab19d40305e2426f54d3c184914889966fdbf50
Bruno <bruno.silva.castilho@gmail.com>
2025-05-21 13:26:22 -0300

    feat: login form schema

--- front-end/src/pages/login/index.tsx
+++ front-end/src/pages/login/index.tsx
@@ -11,9 +11,39 @@ import {
 
 import { useState } from 'react'
 import ForgotPassword from '../../components/ForgotPassword'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+
+export const LoginFormSchema = z.object({
+  email: z.string().trim().email({ message: 'E-mail inválido' }),
+  password: z
+    .string()
+    .trim()
+    .min(8, { message: 'A senha deve ter no mínimo 8 caracteres' })
+    .regex(/(?=.*[A-Z])/, {
+      message: 'A senha deve conter pelo menos uma letra maiúscula',
+    })
+    .regex(/(?=.*[0-9])/, {
+      message: 'A senha deve conter pelo menos um número',
+    })
+    .regex(/(?=.*[!@#$%^&*(),.?":{}|<>])/, {
+      message: 'A senha deve conter pelo menos um símbolo especial',
+    }),
+})
+
+type LoginFormSchemaType = z.infer<typeof LoginFormSchema>
 
 export function Login() {
   const [open, setOpen] = useState(false)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<LoginFormSchemaType>({
+    resolver: zodResolver(LoginFormSchema),
+  })
 
   const handleClickOpen = () => {
     setOpen(true)
@@ -23,6 +53,10 @@ export function Login() {
     setOpen(false)
   }
 
+  function handleSubmitForm(data: LoginFormSchemaType) {
+    console.log(data)
+  }
+
   return (
     <>
       <Typography component="h1" variant="h4">
@@ -30,7 +64,7 @@ export function Login() {
       </Typography>
       <Box
         component="form"
-        noValidate
+        onSubmit={handleSubmit(handleSubmitForm)}
         sx={{
           display: 'flex',
           flexDirection: 'column',
@@ -44,32 +78,40 @@ export function Login() {
             size="small"
             id="email"
             type="email"
-            name="email"
             placeholder="your@email.com"
-            autoComplete="email"
             autoFocus
             required
             fullWidth
             variant="outlined"
+            error={!!errors.email}
+            helperText={errors.email?.message ?? ''}
+            {...register('email')}
           />
         </FormControl>
         <FormControl>
           <FormLabel htmlFor="password">Senha</FormLabel>
           <TextField
             size="small"
-            name="password"
             placeholder="••••••"
             type="password"
             id="password"
-            autoComplete="current-password"
             autoFocus
             required
             fullWidth
             variant="outlined"
+            error={!!errors.password}
+            helperText={errors.password?.message ?? ''}
+            {...register('password')}
           />
         </FormControl>
         <ForgotPassword open={open} handleClose={handleClose} />
-        <Button type="submit" size="small" fullWidth variant="contained">
+        <Button
+          type="submit"
+          size="small"
+          fullWidth
+          variant="contained"
+          disabled={isSubmitting}
+        >
           Entrar
         </Button>
         <Link
@@ -86,7 +128,7 @@ export function Login() {
       <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
         <Typography sx={{ textAlign: 'center' }}>
           Não tem uma conta?{' '}
-          <Link href="/" variant="body2" sx={{ alignSelf: 'center' }}>
+          <Link href="/cadastrar" variant="body2" sx={{ alignSelf: 'center' }}>
             Inscrever-se
           </Link>
         </Typography>
